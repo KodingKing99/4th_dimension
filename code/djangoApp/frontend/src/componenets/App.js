09a8cd292f0f4a7769e089b408d645044b45f67e
Nick Sorenson <nicksorenson99@gmail.com>
2021-10-14 16:18:37 -0600

    React setup, I'm getting a compile error and i'm not sure why...

---
+++ code/djangoApp/frontend/src/componenets/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import {Router} from 'react-router-dom';
+// const App = (props) => {
+//     return (
+//         <div>Hello!</div>
+//      );
+// }
+class App extends React.Component{
+    render(){
+        return <div>Hello!</div>
+    }
+}
+ReactDOM.render(<App/>, document.getElementById('app'))
+// export default App;

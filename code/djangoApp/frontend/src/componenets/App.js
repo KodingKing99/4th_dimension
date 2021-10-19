747fd7f7f9749e8f3318f34afc11707babc8f74b
Nick Sorenson <nicksorenson99@gmail.com>
2021-10-18 18:11:15 -0600

    Made very very simple Home componenet

--- code/djangoApp/frontend/src/componenets/App.js
+++ code/djangoApp/frontend/src/componenets/App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-// import {Router} from 'react-router-dom';
-// const App = (props) => {
-//     return (
-//         <div>Hello!</div>
-//      );
-// }
-class App extends React.Component{
-    render(){
-        return <div>Hello!</div>
-    }
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './Home';
+
+function App() {
+  return (
+      <div>
+          <Home/>
+      </div>
+  );
 }
 ReactDOM.render(<App/>, document.getElementById('app'))
-// export default App;
+      
+    // <Router>    
+    //   <div className="App">
+    //     <div className="content">
+    //       <Switch>
+    //         <Route exact path="/">
+    //           <Home/>
+    //         </Route>
+    //         {/* <Route path="/create">
+    //           <Create/>
+    //         </Route>
+    //         <Route path="/blogs/:id">
+    //           <BlogDetails/>
+    //         </Route> 
+    //         <Route path="*">
+    //           <NotFound/>
+    //         </Route> */}
+    //       </Switch>
+    //     </div>
+    //   </div>
+    // </Router>

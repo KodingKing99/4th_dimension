082c0da8a380bff46ea6b833b2d380591dd93e5b
Trax Bagley <kf7mxe@gmail.com>
2021-10-20 23:04:36 -0600

    Added basic bottom navigation bar functionallity that goes to page for that item. Navigation bar changes depending on role

---
+++ code/djangoApp/frontend/src/componenets/Pages/GamePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import './Home.css'
+const GamePage = (props) => {
+    return ( 
+        <div className="homeTop" style={{marginTop: '100px'}}>
+            <p>In Game View</p>
+                    </div>
+     );
+}
+ 
+export default GamePage;
---
+++ code/djangoApp/frontend/src/componenets/Pages/ManageUsersPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import './Home.css'
+const ManageUsersPage = (props) => {
+    return ( 
+        <div className="homeTop" style={{marginTop: '100px'}}>
+            <p>In Game View</p>
+                    </div>
+     );
+}
+ 
+export default ManageUsersPage;

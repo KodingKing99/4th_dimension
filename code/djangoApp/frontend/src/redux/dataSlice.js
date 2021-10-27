ce87b6ddd8b3689154a4d26002cfb6c9ef4549de
nicksorenson <nicksorenson99@gmail.com>
2021-10-27 12:45:35 -0600

    Working on thunk stuff

---
+++ code/djangoApp/frontend/src/redux/dataSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { getAllTournaments } from '../services/services';
+
+const initialState = {
+  tournaments : [],
+}
+
+export const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {
+    setTournaments : (state, action) => {
+        state.tournaments = action.payload;
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { setTournaments } = dataSlice.actions
+
+export default dataSlice.reducer
+// export const fetchTournaments = () => async (dispatch) => {
+//     const response = getAllTournaments();
+//     dispatch(setTournaments(response.data))
+// }
--- code/djangoApp/frontend/src/services/services.js
+++ code/djangoApp/frontend/src/services/services.js
@@ -1,14 +1,25 @@
 import useFetch from "react-fetch-hook"
+import store from '../redux/store'
+// import { useDispatch } from "react-redux";
+import { setTournaments } from "../redux/dataSlice";
 import axios from "axios";
 const applicationName = 'http://127.0.0.1:8000/api/'
 let requestOptions = {
     method : 'POST',
     headers: { 'Content-Type': 'application/json' },
 }
+// const dispatch = useDispatch();
 export const getAllTournaments = () => {
-    console.log("Getting all tournaments...")
-    const {data, isLoading, error} = useFetch(applicationName + 'tournament/');
-    return {data, isLoading, error}
+    // console.log("Getting all tournaments...")
+    // const {data, isLoading, error} = useFetch(applicationName + 'tournament/');
+    // return {data, isLoading, error}
+    axios.get(applicationName + 'tournament/').then((response) => {
+        console.log("Fetched tourneys")
+        console.log(response);
+        console.log(store)
+        return response
+        // store.dispatch({type: setTournaments, payload: {response}})
+    }).catch(err => console.log(err));
 }
 export const createTournament = (date, sponsorId, prize, holeCount) => {  
     let data = {

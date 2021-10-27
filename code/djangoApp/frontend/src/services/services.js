38868708ff7c8703566c2ed67a0c4380c0c847db
nicksorenson <nicksorenson99@gmail.com>
2021-10-27 11:57:01 -0600

    have some server stuff working, now I need to just do more

--- code/djangoApp/frontend/src/services/services.js
+++ code/djangoApp/frontend/src/services/services.js
@@ -1,4 +1,5 @@
 import useFetch from "react-fetch-hook"
+import axios from "axios";
 const applicationName = 'http://127.0.0.1:8000/api/'
 let requestOptions = {
     method : 'POST',
@@ -10,15 +11,13 @@ export const getAllTournaments = () => {
     return {data, isLoading, error}
 }
 export const createTournament = (date, sponsorId, prize, holeCount) => {  
-    requestOptions = {
-        ...requestOptions,
-        body: JSON.stringify({ 
-            tournamentdate: date,
-            tournamnetsponsor: sponsorId,
-            tournamentprize: prize,
-            tournamentholecount: holeCount
-        })
-    };
-    const {data, isLoading, error} = useFetch(applicationName + 'tournament/', {...requestOptions});
-    return {data, isLoading, error};
+    let data = {
+        tournamentdate: date,
+        tournamnetsponsor: sponsorId,
+        tournamentprize: prize,
+        tournamentholecount: holeCount
+    }
+    axios.post(applicationName + 'tournament/', data).then((response) => {
+        console.log(response);
+    })
 }

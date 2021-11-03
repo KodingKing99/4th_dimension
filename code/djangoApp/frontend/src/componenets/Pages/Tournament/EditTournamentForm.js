ea1ac3775a8fec9a0aabaf85b1ae4b3ebe3e8282
Nick Sorenson <nicksorenson99@gmail.com>
2021-11-03 12:08:11 -0600

    Front end for edit tourney form is looking pretty good

--- code/djangoApp/frontend/src/componenets/Pages/Tournament/EditTournamentForm.js
+++ code/djangoApp/frontend/src/componenets/Pages/Tournament/EditTournamentForm.js
@@ -4,12 +4,18 @@ import { Fade, Modal, Tabs, Tab, Typography } from '@mui/material';
 // import { Box } from '@mui/system';
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import { Button, Container, Stack } from "@mui/material";
+// import { Box } from "@mui/system";
 // import Typography from '@mui/material/Typography';
 // import Modal from '@mui/material/Modal';
-
+import "./EditTournamentForm.css";
 
 const EditTournamentForm = (props) => {
+    const cssMarginFlexTextAlign = {
+        margin:'5px',
+        display:'flex',
+        textAlign:'center'
+      }
     const style = {
         position: 'absolute',
         top: '50%',
@@ -26,12 +32,10 @@ const EditTournamentForm = (props) => {
     const handleChange = (e, value) => {
         setValue(value);
     }
-    // const [date, setDate] = useState(tourney.tournamentdate);
-    // const [prize, setPrize] = useState(tourney.tournamentprize);
-    // const [holeCount, setHoleCount] = useState(tourney.tournamentholecount);
-    // const [sponsorId, setSponsorId] = useState(tourney.tournamentsponsor);
-    console.log("Hello!")
-    console.log(show);
+    const [date, setDate] = useState();
+    const [prize, setPrize] = useState();
+    const [holeCount, setHoleCount] = useState();
+    const [sponsorId, setSponsorId] = useState();
     return ( 
         <>
         {/* <form>
@@ -56,13 +60,33 @@ const EditTournamentForm = (props) => {
                 <TabContext value={value}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        <Tab label="Item One" value="1" />
+                        <Tab label="Edit" value="1" />
                         <Tab label="Item Two" value="2" />
                         <Tab label="Item Three" value="3" />
                     </TabList>
                     </Box>
                     <TabPanel value="1">
-                        Item One
+                    <Stack sx={{
+                        alignItems: 'center',marginBottom:'10px'}}>
+                        <h1>Edit Tournament</h1>
+                        <h2># Holes</h2>
+                        <Box sx={cssMarginFlexTextAlign} hint="0">
+                            <input className="formInput" type="number" onChange={(e) => setHoleCount(e.target.value)} required/>
+                        </Box>
+                        <h2>Date</h2>
+                        <Box sx={cssMarginFlexTextAlign}>
+                            <input className="formInput" type="datetime-local" onChange={(e) => setDate(e.target.value)} required></input>
+                        </Box>
+                        <h2>Prize</h2>
+                        <Box sx={cssMarginFlexTextAlign}>
+                            <input className="formInput" type="number" required onChange={(e) => setPrize(e.target.value)}></input>
+                        </Box>
+                        <h2>Sponsor ID</h2>
+                        <Box sx={cssMarginFlexTextAlign}>
+                            <input className="formInput" type="number" required onChange={(e) => setSponsorId(e.target.value)}></input>
+                        </Box>
+                        <Button>Submit</Button>
+                    </Stack>
                     </TabPanel>
                     <TabPanel value="2">Item Two</TabPanel>
                     <TabPanel value="3">Item Three</TabPanel>

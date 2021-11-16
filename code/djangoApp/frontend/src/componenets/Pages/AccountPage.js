76ab48416a4536ef3c2bc2ee124c6920addbaa12
clbHrrs <caleb0harris@gmail.com>
2021-11-16 14:05:07 -0700

    fixed deposit money update issue

--- code/djangoApp/frontend/src/componenets/Pages/AccountPage.js
+++ code/djangoApp/frontend/src/componenets/Pages/AccountPage.js
@@ -25,106 +25,111 @@ const style = {
 
 const AccountPage = (props) => {
     const user = useSelector((state) => state.user);
-    const account = useSelector((state) => state.user.account);
+    const [userAccount, setUserAccount] = useState(user.account);
+    const [userFirst, setUserFirst] = useState(user.firstName);
+    const [userLast, setUserLast] = useState(user.lastName);
     return ( 
         <div className="account-page" style={{marginTop: '100px', textAlign: 'center'}}>
             <h1> Account Details </h1>
-            <h2> Name: {user.firstName} {user.lastName}</h2>
+            <h2> Name: {userFirst} {userLast}</h2>
             <h2> Email: {user.email} </h2>
-            <h2> Account Balance: ${account}</h2>
+            <h2> Account Balance: ${userAccount}</h2>
 
             <DepositModal />
             <NameModal />
             
         </div>
-     );
-}
-
-function DepositModal() {
-    const user = useSelector((state) => state.user);
-    const [open, setOpen] = React.useState(false);
-    const [amount, setAmount] = useState(0);
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleSubmit = () => {
-        depositMoney(user.id, amount);
-        setOpen(false);
-    };
-
-    return (
-        <div>
-            <Button variant="outlined" sx={{ margin: "3px" }} onClick={() => {
-                handleOpen();
-            }}>Deposit Money</Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-            >
-                <Box sx={style}>
-                    <Stack sx={{
-                        alignItems: 'center', marginBottom: '10px'
-                    }}>
-                        <h2>Amount to Deposit</h2>
-                        <Box sx={cssMarginFlexTextAlign}>
-                            <input className="formInput" type="number" value={amount} onChange={(e) => setAmount(e.target.value)} required></input>
-                        </Box>
-                        <Button variant="outlined" onClick={() => { handleSubmit(); }}>Submit</Button>
-                    </Stack>
-                </Box>
-            </Modal>
-        </div>
     );
+
+    function DepositModal() {
+        const user = useSelector((state) => state.user);
+        const [open, setOpen] = React.useState(false);
+        const [amount, setAmount] = useState(0);
+        const handleOpen = () => {
+            setOpen(true);
+        };
+        const handleClose = () => {
+            setOpen(false);
+        };
+        const handleSubmit = () => {
+            depositMoney(user.id, amount);
+            setOpen(false);
+            setUserAccount((parseFloat(userAccount) + parseFloat(amount)).toFixed(2));
+        };
+
+        return (
+            <div>
+                <Button variant="outlined" sx={{ margin: "3px" }} onClick={() => {
+                    handleOpen();
+                }}>Deposit Money</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                >
+                    <Box sx={style}>
+                        <Stack sx={{
+                            alignItems: 'center', marginBottom: '10px'
+                        }}>
+                            <h2>Amount to Deposit</h2>
+                            <Box sx={cssMarginFlexTextAlign}>
+                                <input className="formInput" type="number" value={amount} onChange={(e) => setAmount(e.target.value)} required></input>
+                            </Box>
+                            <Button variant="outlined" onClick={() => { handleSubmit(); }}>Submit</Button>
+                        </Stack>
+                    </Box>
+                </Modal>
+            </div>
+        );
+    }
+
+    function NameModal() {
+        const user = useSelector((state) => state.user);
+        const [open, setOpen] = React.useState(false);
+        const [firstName, setFirstName] = useState(user.firstName);
+        const [lastName, setLastName] = useState(user.lastName);
+        const handleOpen = () => {
+            setOpen(true);
+        };
+        const handleClose = () => {
+            setOpen(false);
+        };
+        const handleSubmit = () => {
+            changeName(user.id, firstName, lastName);
+            setOpen(false);
+            setUserFirst(firstName);
+            setUserLast(lastName);
+        };
+
+        return (
+            <div>
+                <Button variant="outlined" sx={{ margin: "3px" }} onClick={() => {
+                    handleOpen();
+                }}>Change Name</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                >
+                    <Box sx={style}>
+                        <Stack sx={{
+                            alignItems: 'center', marginBottom: '10px'
+                        }}>
+                            <h2>New First Name</h2>
+                            <Box sx={cssMarginFlexTextAlign}>
+                                <input className="formInput" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required></input>
+                            </Box>
+                            <h2>New Last Name</h2>
+                            <Box sx={cssMarginFlexTextAlign}>
+                                <input className="formInput" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required></input>
+                            </Box>
+                            <Button variant="outlined" onClick={() => { handleSubmit(); }}>Submit</Button>
+                        </Stack>
+                    </Box>
+                </Modal>
+            </div>
+        );
+    }
 }
 
-function NameModal() {
-    const user = useSelector((state) => state.user);
-    const [open, setOpen] = React.useState(false);
-    const [firstName, setFirstName] = useState(user.firstName);
-    const [lastName, setLastName] = useState(user.lastName);
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleSubmit = () => {
-        changeName(user.id, firstName, lastName);
-        setOpen(false);
-    };
-
-    return (
-        <div>
-            <Button variant="outlined" sx={{ margin: "3px" }} onClick={() => {
-                handleOpen();
-            }}>Change Name</Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-            >
-                <Box sx={style}>
-                    <Stack sx={{
-                        alignItems: 'center', marginBottom: '10px'
-                    }}>
-                        <h2>New First Name</h2>
-                        <Box sx={cssMarginFlexTextAlign}>
-                            <input className="formInput" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required></input>
-                        </Box>
-                        <h2>New Last Name</h2>
-                        <Box sx={cssMarginFlexTextAlign}>
-                            <input className="formInput" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required></input>
-                        </Box>
-                        <Button variant="outlined" onClick={() => { handleSubmit(); }}>Submit</Button>
-                    </Stack>
-                </Box>
-            </Modal>
-        </div>
-    );
-}
- 
 export default AccountPage;
